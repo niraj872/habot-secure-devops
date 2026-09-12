0d34aad8662e10f0e7c1341c2b7455ee78258953
Niraj Yadav <YOUR_GITHUB_EMAIL>
2026-09-12 12:25:46 +0530

    fix: finalize CI Python quality gates

--- backend/manage.py
+++ backend/manage.py
@@ -1,4 +1,4 @@
-﻿# Author: NIRAJ KR YADAV
+# Author: NIRAJ KR YADAV
 # Email: Nirajyadav9466@gmail.com
 # Phone: 7366913096
 
@@ -9,9 +9,9 @@ import sys
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "habot_project.settings")
     from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
     main()
-
